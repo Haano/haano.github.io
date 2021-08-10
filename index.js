820c940e887b46c41b626f5ed1a54f7ac1640a42
Haano <85753285+Haano@users.noreply.github.com>
2021-08-10 17:02:37 +0300

    dsaf

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const todoRoutes = require('./routes/todos')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5500
 
 const app = express()
 const hbs = exphbs.create({
-    defaultLayout: 'index',
+    defaultLayout: 'Main',
     extname: 'hbs'
 })
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
 
 app.use(todoRoutes)
 
--- routes/todos.js
+++ routes/todos.js
@@ -1,12 +1,38 @@
 const { Router } = require('express')
+const Todo = require('../models/Todo')
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.render('index')
+router.get('/', async(req, res) => {
+    const todos = await Todo.find({})
+
+    res.render('index', {
+        title: 'Todos list',
+        isIndex: true,
+        todos
+    })
 })
 
 router.get('/create', (req, res) => {
-    res.render('create')
+    res.render('create', {
+        title: 'Create',
+        isCreate: true
+    })
 })
+
+router.get('/TestSite', (req, res) => {
+    res.render('TestSite', {
+        title: 'TestSite'
+    })
+})
+
+router.post('/create', async(req, res) => {
+    const todo = new Todo({
+        title: req.body.title
+    })
+
+    await todo.save()
+    res.redirect('/')
+})
+
 
 module.exports = router
